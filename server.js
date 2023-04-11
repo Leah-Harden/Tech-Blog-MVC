d89c6c9f32dc0c3500a2bb6880e6a3ee75bfe3c0
Leah <leahxharden@gmail.com>
2023-04-11 15:33:34 -0400

    added required

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
+const User = require('./User');
+
+module.exports = {
+    User
+};
+
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
