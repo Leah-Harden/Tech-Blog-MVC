7740452636de200d73d5667a88639c3810d75b3b
Leah <leahxharden@gmail.com>
2023-04-21 17:00:27 -0400

    Working handlebars WIP

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,7 +7,9 @@ const { User } = require('../models');
 router.get('/', async (req, res) => {
     try {
     const userData = await User.findAll();
+    const userData = await User.findByPk(req.session.user_id);
     const users = userData.map((user) => user.get({ plain: true }));
+    const users = userData.get({ plain: true });
     res.render('login', { 
         logged_in: req.session.logged_in 
     });
@@ -18,13 +20,12 @@ router.get('/', async (req, res) => {
 
 router.get('/dashboard', async (req, res) => {
     try {
-    const userData = await User.findAll({
-        include: [
-        ],
-    });
-    const users = userData.map((user) => user.get({ plain: true }));
+    const userData = await User.findByPk(req.session.user_id);
+
+    const users = userData.get({ plain: true });
     res.render('dashboard', { 
-        logged_in: req.session.logged_in 
+        logged_in: req.session.logged_in,
+        users
     });
     } catch(err) {
         console.log(err)
@@ -33,13 +34,12 @@ router.get('/dashboard', async (req, res) => {
 
 router.get('/home', async (req, res) => {
     try {
-    const userData = await User.findAll({
-        include: [
-        ],
-    });
-    const users = userData.map((user) => user.get({ plain: true }));
+        const userData = await User.findByPk(req.session.user_id);
+
+        const users = userData.get({ plain: true });
     res.render('home', { 
-        logged_in: req.session.logged_in 
+        logged_in: req.session.logged_in,
+        users
     });
     } catch(err) {
         console.log(err)
