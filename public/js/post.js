8a80f0aee0135d8286d1e0bf86b6978f1f22b97d
Leah <leahxharden@gmail.com>
2023-04-27 16:18:47 -0400

    working on savePost WIP

--- public/js/post.js
+++ public/js/post.js
@@ -4,40 +4,52 @@ const flexcol = document.querySelector('#flexcol')
 
 
 
-const savePost = (post) =>{
-    try{
-        
+const savePost = (post) => {
+    try {
+
         fetch('/api/post', {
             method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(post),
-    })
-} catch (err){
-    console.log(err)
-}    
-}
-    
-    
-const handlePostSave = (e) => {
-    preventDefault(e) 
-    try{
-
-        const newPost = {
-            title: postTitle.value,
-            user_id: postUser.value,
-            body: postbody.value,
-        };
-        savePost(newPost).then(() => {
-            createPost(newPost)
-            
-        });
-    } catch (err){
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(post),
+        })
+    } catch (err) {
         console.log(err)
     }
-};
+}
 
+
+// const handlePostSave = (e) => {
+//     preventDefault(e) 
+//     try{
+
+//         const newPost = {
+//             title: postTitle.value,
+//             user_id: postUser.value,
+//             body: postbody.value,
+//         };
+//         savePost(newPost).then(() => {
+//             createPost(newPost)
+
+//         });
+//     } catch (err){
+//         console.log(err)
+//     }
+// };
+
+const newFormHandler = async function (event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const user = document.querySelector('h2[name="navUser"]').value;
+    const body = document.querySelector('textarea[name="post-body"]').value;
+    console.log(title, user, body)
+    savePost(title, user, body).then(() => {
+        createPost(title, user, body)
+
+    })
+};
 
 // Makes the Post
 const createPost = ({ title, user, body }) => {
@@ -70,11 +82,11 @@ const createPost = ({ title, user, body }) => {
         flexcol.append(PostCenEl);
 
     }
-    catch (err){
+    catch (err) {
         console.log(err)
     }
 }
 
 
 // Gets notes from the db and renders them to the sidebar
-document.querySelector('.addPost').addEventListener('submit', handlePostSave);
+document.querySelector('.addPost').addEventListener('submit', newFormHandler);
