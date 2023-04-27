433d1f1296d586684517f4c0d81c81aa611f9131
Leah <leahxharden@gmail.com>
2023-04-27 13:06:42 -0400

    added try and catches

--- public/js/post.js
+++ public/js/post.js
@@ -1,54 +1,70 @@
 
 
-const savePost = (post) =>
-    fetch('/api/post', {
-        method: 'POST',
+const savePost = (post) =>{
+    try{
+        
+        fetch('/api/post', {
+            method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(post),
-});
-
-
+    })
+} catch (err){
+    console.log(err)
+}    
+}
+    
+    
 const handlePostSave = () => {
-    const newPost = {
-        title: postTitle.value,
-        user_id: postUser.value,
-        body: postbody.value,
-    };
-    savePost(newPost).then(() => {
-        createPost(newPost)
+    try{
 
-    });
+        const newPost = {
+            title: postTitle.value,
+            user_id: postUser.value,
+            body: postbody.value,
+        };
+        savePost(newPost).then(() => {
+            createPost(newPost)
+            
+        });
+    } catch (err){
+        console.log(err)
+    }
 };
 
 
 // Makes the Post
-const createPost = ({title, user, body}) => {
+const createPost = ({ title, user, body }) => {
+
+    try {
         // add the elements
-    const PostCenEl = document.createElement('div');
-    PostCenEl.classList.add('postCenter');
-    const PostEl = document.createElement('div');
-    PostEl.classList.add('post');
-    const postTitle = document.createElement('h2');
-      // --------------------------------
-      // add the text elements
-    postTitle.classList.add('RobotoMono postTitle');
-    const postUsername = document.createElement('h2');
-    postUsername.classList.add('RobotoMono postUsername');
-    const postBody = document.createElement('p');
-    postBody.classList.add('RobotoMono postBody');
-    const postButton = document.createElement('button');
-    postButton.classList.add('RobotoMono postBtn');
-    // add the texts in
-    postTitle.innerText = title;
-    postUsername.innerText = user;
-    postBody.innerText = body;
-    delBtnEl.addEventListener('click', handleNoteDelete);
-};
+        const PostCenEl = document.createElement('div');
+        PostCenEl.classList.add('postCenter');
+        const PostEl = document.createElement('div');
+        PostEl.classList.add('post');
+        const postTitle = document.createElement('h2');
+        // --------------------------------
+        // add the text elements
+        postTitle.classList.add('RobotoMono postTitle');
+        const postUsername = document.createElement('h2');
+        postUsername.classList.add('RobotoMono postUsername');
+        const postBody = document.createElement('p');
+        postBody.classList.add('RobotoMono postBody');
+        const postButton = document.createElement('button');
+        postButton.classList.add('RobotoMono postBtn');
+        // add the texts in
+        postTitle.innerText = title;
+        postUsername.innerText = user;
+        postBody.innerText = body;
+        delBtnEl.addEventListener('click', handleNoteDelete);
+    } catch (err) {
+        console.log(err)
+    }
+}
 
 
 // Gets notes from the db and renders them to the sidebar
-const addPostHome = document.querySelector('addPost').addEventListener('submit',handlePostSave);
+const addPostHome = document.querySelector('addPost').addEventListener('submit', handlePostSave);
 
 getAndRenderPosts()
