2cddee2e17b539b571ac59f6f36e1f4d922477b3
Leah <leahxharden@gmail.com>
2023-05-05 21:59:55 -0400

    update

--- public/js/post.js
+++ public/js/post.js
@@ -31,66 +31,66 @@ const savePost = (post) => {
 }
 
 
-
-// Makes the Post
-const createPost = ({ title, user, body }) => {
-    try {
-        // add the elements
-        const PostCenEl = document.createElement('div');
-        PostCenEl.classList.add('postCenter');
-        const PostEl = document.createElement('div');
-        PostEl.classList.add('post');
-        const postTitle = document.createElement('h2');
-        PostCenEl.append(PostEl);
-        PostEl.append(postTitle);
-        // --------------------------------
-        // add the text elements
-        postTitle.classList.add('RobotoMono','postTitle');
-        const postDate = document.createElement('h2');
-        postDate.classList.add('RobotoMono','postUsername');
-        const postUsername = document.createElement('h2');
-        postUsername.classList.add('RobotoMono','postUsername');
-        const postBody = document.createElement('p');
-        postBody.classList.add('RobotoMono','postBody');
-        PostEl.append(postDate);
-        PostEl.append(postUsername);
-        PostEl.append(postBody);
-        // --------------------------------
-        //add the comment elements 
-        const formComment  = document.createElement('form');
-        const textareaComment = document.createElement('textarea');
-        textareaComment.classList.add('RobotoMono','postComment','commentInput');
-        const CommentBtn = document.createElement('button');
-        CommentBtn.classList.add('RobotoMono','commentBtn');
-        formComment.append(textareaComment);
-        formComment.append(CommentBtn);
-        PostEl.append(formComment);
-        //add the comment elements 
+// rip ;-;
+// // Makes the Post
+// const createPost = ({ title, user, body }) => {
+//     try {
+//         // add the elements
+//         const PostCenEl = document.createElement('div');
+//         PostCenEl.classList.add('postCenter');
+//         const PostEl = document.createElement('div');
+//         PostEl.classList.add('post');
+//         const postTitle = document.createElement('h2');
+//         PostCenEl.append(PostEl);
+//         PostEl.append(postTitle);
+//         // --------------------------------
+//         // add the text elements
+//         postTitle.classList.add('RobotoMono','postTitle');
+//         const postDate = document.createElement('h2');
+//         postDate.classList.add('RobotoMono','postUsername');
+//         const postUsername = document.createElement('h2');
+//         postUsername.classList.add('RobotoMono','postUsername');
+//         const postBody = document.createElement('p');
+//         postBody.classList.add('RobotoMono','postBody');
+//         PostEl.append(postDate);
+//         PostEl.append(postUsername);
+//         PostEl.append(postBody);
+//         // --------------------------------
+//         //add the comment elements 
+//         const formComment  = document.createElement('form');
+//         const textareaComment = document.createElement('textarea');
+//         textareaComment.classList.add('RobotoMono','postComment','commentInput');
+//         const CommentBtn = document.createElement('button');
+//         CommentBtn.classList.add('RobotoMono','commentBtn');
+//         formComment.append(textareaComment);
+//         formComment.append(CommentBtn);
+//         PostEl.append(formComment);
+//         //add the comment elements 
         
-        // create the date  --------------------------------
+//         // create the date  --------------------------------
         
-        const d = new Date();
-        let day = d.getDate();
-        let month = d.getMonth();
-        let year = d.getFullYear()
-        date = `${year}-${month}-${day}`
+//         const d = new Date();
+//         let day = d.getDate();
+//         let month = d.getMonth();
+//         let year = d.getFullYear()
+//         date = `${year}-${month}-${day}`
         
-        //create the date  --------------------------------
-        // add the texts in
-        postTitle.innerText = title;
-        postUsername.innerText = user;
-        postBody.innerText = body;
-        postDate.innerText = date;
-        CommentBtn.innerHTML = 'comment';
-        // append everythings
-        console.log(flexcol)
-        centerHome.prepend(PostCenEl);
-        console.log(PostCenEl)
-    }
-    catch (err) {
-        console.log(err)
-    }
-}
+//         //create the date  --------------------------------
+//         // add the texts in
+//         postTitle.innerText = title;
+//         postUsername.innerText = user;
+//         postBody.innerText = body;
+//         postDate.innerText = date;
+//         CommentBtn.innerHTML = 'comment';
+//         // append everythings
+//         console.log(flexcol)
+//         centerHome.prepend(PostCenEl);
+//         console.log(PostCenEl)
+//     }
+//     catch (err) {
+//         console.log(err)
+//     }
+// }
 
 
 
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,25 +1,20 @@
-const bcrypt = require('bcrypt');
 const Handlebars = require('Handlebars');
 
 
-module.exports = function ifeq (Handlebars) {
-    Handlebars.registerHelper('ifeq', function (a, b, options) {
-        if (a == b) { 
-            return options.fn(this);
-        }
-        return options.inverse(this);
-        
-    });
-}    
-    
+module.exports = {
+
+    ifeq: test => {
+        (a, b, options) => {
+            if (a == b) {
+                return options.fn(this);
+            }
+            return options.inverse(this);
+
+        };
+        return test;
+    }
+}
 
 
-// // add  bcrypt 
-// const salt = bcrypt.genSalt()
-// bcrypt.genSalt(10, function(err, salt) {
-//     bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
-//         // Store hash in your password DB.
-//     });
-// });
 
 
